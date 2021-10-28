b3f2e359095c5d15d273a1a0dd606bcd1198f874
mcken-vince <mcken.vince@gmail.com>
2021-10-28 13:47:06 -0600

    Styling daily forecast card

--- src/components/DailyForecastCard.tsx
+++ src/components/DailyForecastCard.tsx
@@ -5,8 +5,12 @@ const DailyForecastCard = (props: IDailyForecast) => {
   
   return (
     <div className='daily-forecast-card-container'>
-      <p>{props.day}</p>
-      <img src={props.icon} alt={props.alt}/>
+      <p className='daily-forecast-card-header'>{props.day}</p>
+      <img className='daily-forecast-card-image' src={props.icon} alt={props.alt}/>
+      <div className='daily-forecast-card-footer'>
+        <p>{Math.round(props.maxTemp)}°</p>
+        <p>{Math.round(props.minTemp)}°</p>
+      </div>
     </div>
   );
 };
