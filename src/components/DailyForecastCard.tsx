e492931704dc880302806c6dd7fdda1013d6f57d
mcken-vince <mcken.vince@gmail.com>
2021-10-28 13:56:04 -0600

    Install classnames, show grey background for today's card, display correct day of week for today

--- src/components/DailyForecastCard.tsx
+++ src/components/DailyForecastCard.tsx
@@ -1,11 +1,16 @@
 import { IDailyForecast } from '../types';
 import '../styles/DailyForecastCard.scss';
+import classNames from 'classnames';
 
 const DailyForecastCard = (props: IDailyForecast) => {
   
+  const containerClass:string = classNames('daily-forecast-card-container', {today: (props.day === 0)});
+  // This only works for today
+  const today = new Date().toDateString().split(' ')[0];
+
   return (
-    <div className='daily-forecast-card-container'>
-      <p className='daily-forecast-card-header'>{props.day}</p>
+    <div className={containerClass}>
+      <p className='daily-forecast-card-header'>{today}</p>
       <img className='daily-forecast-card-image' src={props.icon} alt={props.alt}/>
       <div className='daily-forecast-card-footer'>
         <p>{Math.round(props.maxTemp)}°</p>
