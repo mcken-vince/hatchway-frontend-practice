8075d016b5adab36eca55339e5a3951130ec64e7
mcken-vince <mcken.vince@gmail.com>
2021-10-28 13:06:15 -0600

    Add day index to forecast, only return 5 days total

--- src/hooks/weatherForecast.ts
+++ src/hooks/weatherForecast.ts
@@ -1,22 +1,26 @@
 import axios from 'axios';
+import { timeStamp } from 'console';
 import { useEffect, useState } from 'react';
 
 const calgaryLatitude: string = '51.049999';
 const calgaryLongitude: string = '-114.066666'
 
 const useWeatherData = () => {
-  const [weatherData, setWeatherData] = useState<any>({});
+  const [weatherData, setWeatherData] = useState<any>(null);
   const uri: string = `http://api.openweathermap.org/data/2.5/onecall?lat=${calgaryLatitude}&lon=${calgaryLongitude}&exclude=minutely,hourly,alerts&units=metric&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
 
   useEffect(() => {
     axios.get(uri)
     .then(response => {
+      const timezoneOffset = response.data.timezone_offset;
       const dailyForecast: any[] = response.data.daily;
-      const formattedForecast = dailyForecast.map(day => {
+      const formattedForecast = dailyForecast.map((day, idx) => {
         // Returns an object with only the needed information
-        return ({date: day.dt, minTemp: day.temp.min, maxTemp: day.temp.max, icon: day.weather[0].icon})
+        // Only today and next 4 days
+        if (idx < 5) {
+          return ({day: idx, dt: day.dt - timezoneOffset, minTemp: day.temp.min, maxTemp: day.temp.max, icon: day.weather[0].icon})
+        }
       })
-
       setWeatherData(formattedForecast);
     })
     .catch(err => console.error('Error fetching weather forecast from API'));
